c604fb9c054b840cab2d443eb0927ac824bdf38e
mariano0770 <marianotrobo@gmail.com>
2025-11-08 19:00:16 -0300

    Unifico settings.py para produccion (sin decouple)

--- centro_estetica/settings.py
+++ centro_estetica/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-from decouple import config
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'n7)wp@y+3_k$#92#sfmyeaqufq%(fl@c83lehisd13aq*w@4*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -128,13 +126,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CONFIGURACIÓN PARA ENVÍO DE EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-# Lee el usuario y la contraseña desde el archivo .env
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # CONFIGURACIÓN DE AUTENTICACIÓN
 #LOGIN_REDIRECT_URL = '/turnos/'  # A dónde ir después de un login exitoso (al Dashboard)
